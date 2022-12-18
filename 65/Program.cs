18d44be000d2b3af8ba69380e5705b61e7f34f4f
Артем Шустов <artemmeme2017@yandex.ru>
2022-12-18 19:03:42 +0300

    программа для написания суммы чисел от M до N

--- 65/Program.cs
+++ 65/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int sum = 0;
+Console.WriteLine("Введите число M:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($" {PrintNumRec(N)} ");
+int PrintNumRec(int N)
+{
+
+    if (N < M)
+    {
+        return 0;
+    }
+    else
+    {
+        sum = sum + N  + PrintNumRec(N - 1);
+    }
+    return sum;
+}
